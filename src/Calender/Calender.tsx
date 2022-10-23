533d77e1c6e847f6b927cb26e5eb9c66a3fd098e
takurinton <takurinton@takurinton.dev>
2022-10-23 15:48:53 +0900

    add: infinity loading

--- src/Calender/Calender.tsx
+++ src/Calender/Calender.tsx
@@ -1,6 +1,6 @@
 import dayjs, { Dayjs } from "dayjs";
 import { ScrollArea } from "ingred-ui";
-import { FC, useEffect, useMemo } from "react";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
 import { DatePicker } from "../DatePicker";
 import { Container } from "./styled";
 
@@ -30,9 +30,55 @@ export const Calender: FC<Props> = ({ date = dayjs(), onDateChange }) => {
     d.clone().subtract(12 - i, "month")
   );
 
-  const monthList = [...prevYearMonthList, ...nextYearMonthList];
+  const ref = useRef<HTMLDivElement>(null);
+  const [loaded, setLoaded] = useState<{
+    prev: Dayjs;
+    next: Dayjs;
+  }>({
+    prev: d.clone().subtract(12, "month"),
+    next: d.add(1, "year"),
+  });
+  const [monthList, setMonthList] = useState<Dayjs[]>([
+    ...prevYearMonthList,
+    ...nextYearMonthList,
+  ]);
 
   const vdate = date.clone();
+
+  const handleScrollDown = () => {
+    if (ref.current === null) {
+      return;
+    }
+
+    const { scrollTop, clientHeight, scrollHeight } = ref.current;
+
+    const next = loaded.next.add(1, "year");
+
+    if (scrollTop + clientHeight + 100 >= scrollHeight) {
+      const nextYearMonthList = Array.from(new Array(12)).map((_, i) =>
+        loaded.next.clone().add(i, "month")
+      );
+      setLoaded({ next, prev: loaded.prev });
+      setMonthList([...monthList, ...nextYearMonthList]);
+    }
+  };
+
+  const handleScrollUp = () => {
+    if (ref.current === null) {
+      return;
+    }
+
+    const { scrollTop } = ref.current;
+
+    const prev = loaded.prev.subtract(1, "year");
+    if (scrollTop - 100 <= 0) {
+      const prevYearMonthList = Array.from(new Array(12)).map((_, i) =>
+        loaded.prev.clone().subtract(12 - i, "month")
+      );
+      setLoaded({ next: loaded.next, prev });
+      setMonthList([...prevYearMonthList, ...monthList]);
+    }
+  };
 
   // TODO: SSR support
   useEffect(() => {
@@ -42,9 +88,23 @@ export const Calender: FC<Props> = ({ date = dayjs(), onDateChange }) => {
     }
   }, []);
 
+  useEffect(() => {
+    if (ref.current !== null) {
+      ref.current.addEventListener("scroll", handleScrollUp);
+      ref.current.addEventListener("scroll", handleScrollDown);
+    }
+
+    return () => {
+      if (ref.current !== null) {
+        ref.current.removeEventListener("scroll", handleScrollUp);
+        ref.current.removeEventListener("scroll", handleScrollDown);
+      }
+    };
+  }, [loaded]);
+
   return (
     <Container>
-      <ScrollArea minHeight={HEIGHT} maxHeight={HEIGHT} id="calender">
+      <ScrollArea ref={ref} minHeight={HEIGHT} maxHeight={HEIGHT} id="calender">
         <>
           {monthList.map((m) => (
             <DatePicker
