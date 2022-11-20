ac53e98241088892a82b0d8159490d1e70962fa7
takurinton <takurinton@takurinton.dev>
2022-11-20 15:23:31 +0900

    fix: same logic with clickState end

--- src/DateRangePicker/internal/Day/Day.tsx
+++ src/DateRangePicker/internal/Day/Day.tsx
@@ -28,32 +28,37 @@ export const Day: FC<Props> = memo(
     changeState,
     onClickDate,
     children,
-  }) => {
-    return (
-      <DayContainer
-        selected={selected}
-        isBetween={isBetween}
-        // How can I improve the User Experience?
-        // Answer: impossible
-        onClick={() => {
-          if (clickState === "start") {
+  }) => (
+    <DayContainer
+      selected={selected}
+      isBetween={isBetween}
+      // How can I improve the User Experience?
+      // Answer: impossible
+      onClick={() => {
+        if (clickState === "start") {
+          if (value.isAfter(date.endDate)) {
+            onClickDate?.({
+              startDate: date.endDate,
+              endDate: value,
+            });
+          } else {
             onClickDate?.({
               startDate: value,
               endDate: date.endDate,
             });
-            changeState("end");
-          } else if (clickState === "end") {
-            if (value.isBefore(date.startDate)) {
-              onClickDate?.({ startDate: value, endDate: date.startDate });
-            } else {
-              onClickDate?.({ startDate: date.startDate, endDate: value });
-            }
-            changeState("start");
           }
-        }}
-      >
-        {children}
-      </DayContainer>
-    );
-  }
+          changeState("end");
+        } else if (clickState === "end") {
+          if (value.isBefore(date.startDate)) {
+            onClickDate?.({ startDate: value, endDate: date.startDate });
+          } else {
+            onClickDate?.({ startDate: date.startDate, endDate: value });
+          }
+          changeState("start");
+        }
+      }}
+    >
+      {children}
+    </DayContainer>
+  )
 );
