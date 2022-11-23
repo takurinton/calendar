3421bdf0491aa3e755a4bffed247fe64731bb0ba
takurinton <takurinton@takurinton.dev>
2022-11-23 09:21:36 +0900

    fix: typo and config

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -3,10 +3,10 @@ import { getPrevYearMonthList, getNextYearMonthList } from "../utils";
 
 test("getMonthList", () => {
   const date = dayjs("2021-01-01");
-  const beforMonthList = getPrevYearMonthList(date);
-  expect(beforMonthList).toHaveLength(12);
-  expect(beforMonthList[0].format("YYYY-MM")).toBe("2020-01");
-  expect(beforMonthList[11].format("YYYY-MM")).toBe("2020-12");
+  const beforeMonthList = getPrevYearMonthList(date);
+  expect(beforeMonthList).toHaveLength(12);
+  expect(beforeMonthList[0].format("YYYY-MM")).toBe("2020-01");
+  expect(beforeMonthList[11].format("YYYY-MM")).toBe("2020-12");
 
   const afterMonthList = getNextYearMonthList(date);
   expect(afterMonthList).toHaveLength(12);
